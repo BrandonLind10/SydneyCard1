911cc9a1ccff5e2bf4d7f56938c95e9a945e12ad
Brandon <brandonl271@gmail.com>
2020-02-19 02:55:56 -0800

    d

--- src/SecurityOverLay.js
+++ src/SecurityOverLay.js
@@ -43,8 +43,8 @@ class SecurityOverLay extends React.Component{
         ReactDOM.render(<Card/>, root);
     }
     
-    submit(){
-        
+    submit(e){
+        e.preventDefault();
         if(this.isNameCorrect()){
              this.remove();
              this.showCard();
